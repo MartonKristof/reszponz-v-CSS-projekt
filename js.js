3ed74aa477b7ee4fd69c5c69cb0aa77868f67337
Marton Kristóf <Marton Kristf@30T-TPC16>
2025-03-05 09:59:32 +0100

    Javított hibák és hozzáadott letöltés

--- js.js
+++ js.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () { 
     const links = document.querySelectorAll(".navList a");
     const currentUrl = window.location.pathname.split("/").pop(); 
-    
+
     links.forEach(link => {
         if (link.getAttribute("href") === currentUrl) {
             link.classList.add("active");
         }
     });
 });
+
+function downloadFile() {
+    const link = document.createElement('a');
+    link.href = 'home.png';  // A fájl elérési útvonala
+    link.download = 'home.png';  // A fájl neve, amit letöltenek
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
